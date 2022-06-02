5ee968ce590b1bb772582bbcc5145e2ff85f2bac
Lukas Riha <lukas.riha94@seznam.cz>
2022-06-02 20:03:47 +0200

    Set up dev, build, start script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Actum Backend
+
+This is the backend portion of an interview for the position of fullstack developer at ACTUM Digital.
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import type { Request, Response } from "express";
+import { config } from "dotenv";
+import { middleware } from "./middleware";
+
+config();
+
+console.log("Config loaded: ", process.env.MONGO_URI);
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(middleware);
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("Hello world!");
+  res
+    .json({
+      status: "ok",
+    })
+    .status(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Started on port ${PORT}`);
+});
